69e6a9a0f1ceee209c5ba81bb4ac6bddb89db378
Klan Shen <klan.shen@hotmail.com>
2026-03-15 03:24:34 +0800

    Upgrade footer and add ICP beian

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,39 +6,46 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={`container ${styles.inner}`}>
-        <div className={styles.meta}>
-          <span>
-            © {new Date().getFullYear()} {site.name}. All rights reserved.
-          </span>
+        <div className={styles.left}>
+          <div className={styles.brandBlock}>
+            <span className={styles.copyright}>
+              © {new Date().getFullYear()} {site.name}. All rights reserved.
+            </span>
 
-          <span className={styles.beian}>
-            <a
-              href="https://beian.miit.gov.cn/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              沪ICP备2026008268号
-            </a>
-          </span>
+            <div className={styles.beianGroup}>
+              <span className={styles.beian}>
+                <a
+                  href="https://beian.miit.gov.cn/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  沪ICP备2026008268号
+                </a>
+              </span>
 
-          {/* 公安备案完成后取消注释 */}
-          {/*
-          <span className={styles.beian}>
-            <a
-              href="http://www.beian.gov.cn/portal/registerSystemInfo"
-              target="_blank"
-              rel="noreferrer"
-            >
-              沪公网安备XXXXXXXXXXXX号
-            </a>
-          </span>
-          */}
+              {/* 公安备案完成后，取消注释并替换号码 */}
+              {/*
+              <span className={styles.beian}>
+                <a
+                  href="http://www.beian.gov.cn/portal/registerSystemInfo"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  沪公网安备XXXXXXXXXXXX号
+                </a>
+              </span>
+              */}
+            </div>
+          </div>
         </div>
 
-        <span className={styles.links}>
-          <Link href="/privacy/">隐私政策</Link>
-          <Link href="/terms/">服务条款</Link>
-        </span>
+        <div className={styles.right}>
+          <nav className={styles.links} aria-label="页脚导航">
+            <Link href="/privacy/">隐私政策</Link>
+            <Link href="/terms/">服务条款</Link>
+            <Link href="/contact/">联系我们</Link>
+          </nav>
+        </div>
       </div>
     </footer>
   );
