5d4f493fc1e7c5647c9330e6e709600b4fe14bb5
Klan Shen <klan.shen@hotmail.com>
2026-03-15 03:12:04 +0800

    Add ICP beian

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,40 @@
-import Link from 'next/link';
-import styles from './Footer.module.css';
-import { site } from '@/data/site';
+import Link from "next/link";
+import styles from "./Footer.module.css";
+import { site } from "@/data/site";
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={`container ${styles.inner}`}>
-        <span>© {new Date().getFullYear()} {site.name}. All rights reserved.</span>
+        <div className={styles.meta}>
+          <span>
+            © {new Date().getFullYear()} {site.name}. All rights reserved.
+          </span>
+
+          <span className={styles.beian}>
+            <a
+              href="https://beian.miit.gov.cn/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              沪ICP备2026008268号
+            </a>
+          </span>
+
+          {/* 公安备案完成后取消注释 */}
+          {/*
+          <span className={styles.beian}>
+            <a
+              href="http://www.beian.gov.cn/portal/registerSystemInfo"
+              target="_blank"
+              rel="noreferrer"
+            >
+              沪公网安备XXXXXXXXXXXX号
+            </a>
+          </span>
+          */}
+        </div>
+
         <span className={styles.links}>
           <Link href="/privacy/">隐私政策</Link>
           <Link href="/terms/">服务条款</Link>
